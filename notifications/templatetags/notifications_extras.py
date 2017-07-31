4509a418c67a11b489654936401f16ab1fda7631
Andreea Dima <andreeadima95@gmail.com>
2017-07-31 15:40:54 +0300

    [refs #84119] Improve pagination

--- notifications/templatetags/notifications_extras.py
+++ notifications/templatetags/notifications_extras.py
@@ -1,13 +1,9 @@
 from django import template
 
-from notifications.models import Company
-
 
 register = template.Library()
 
 
 @register.simple_tag
-def get_group_companies(group):
-    """ Returns all companies for the given group
-    """
-    return Company.objects.filter(group=group)
+def get_group_companies(view, group_code):
+    return view.get_companies_by_group(group_code)
