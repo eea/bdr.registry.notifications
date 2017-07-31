83214ea2472a109c17d9b37a9b723b2163699625
Andreea Dima <andreeadima95@gmail.com>
2017-07-31 15:40:54 +0300

    [refs #84119] Improve pagination

--- notifications/views/base.py
+++ notifications/views/base.py
@@ -1,9 +1,25 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.urls import reverse
 from django.views import generic
 
 from notifications import BDR_GROUP_CODE, FGASES_GROUP_CODE
-from notifications.models import STAGE_CLOSED, Cycle, CompaniesGroup, Person
+from notifications.models import STAGE_CLOSED, Cycle, CompaniesGroup, Person, Company
 from notifications.views.breadcrumb import NotificationsBaseView, Breadcrumb
+
+
+class PaginatedDataViewBase:
+    PER_PAGE = 5
+
+    def get_current_page(self, data, page):
+        paginator = Paginator(data, self.PER_PAGE)
+        try:
+            return paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            return paginator.page(1)
+        except EmptyPage:
+            # If page is out of range deliver last page of results.
+            return paginator.page(paginator.num_pages)
 
 
 class DashboardView(NotificationsBaseView, generic.ListView):
@@ -24,7 +40,8 @@ class DashboardView(NotificationsBaseView, generic.ListView):
         return r
 
 
-class CompaniesView(NotificationsBaseView, generic.TemplateView):
+class CompaniesView(NotificationsBaseView, PaginatedDataViewBase,
+                    generic.TemplateView):
     template_name = 'notifications/companies.html'
 
     def breadcrumbs(self):
@@ -37,28 +54,41 @@ class CompaniesView(NotificationsBaseView, generic.TemplateView):
         return breadcrumbs
 
     def get_groups(self):
-        return CompaniesGroup.objects.all()
+        return CompaniesGroup.objects.values('code', 'title')
+
+    def get_companies_by_group(self, group):
+        page = self.request.GET.get("{0}_page".format(group), 1)
+        return self.get_current_page(
+            data=Company.objects.filter(group__code=group)
+                 .prefetch_related('person_set')
+                 .order_by('name'),
+            page=page
+        )
 
 
-class PersonsView(NotificationsBaseView, generic.TemplateView):
+class PersonsView(NotificationsBaseView, PaginatedDataViewBase,
+                  generic.TemplateView):
     template_name = 'notifications/persons.html'
-    page_bdr = 1
-    page_fgas = 1
-
-    def dispatch(self, request, *args, **kwargs):
-        # get current page from request
-        self.page_bdr = request.GET.get('page_bdr', 1)
-        self.page_fgas = request.GET.get('page_fgas', 1)
-
-        return super(PersonsView, self).dispatch(request, *args, **kwargs)
 
     def get_bdr(self):
-        return Person.objects.filter(
-            company__group__code=BDR_GROUP_CODE)
+        page_bdr = self.request.GET.get('page_bdr', 1)
+        return self.get_current_page(
+            data=Person.objects
+                 .filter(company__group__code=BDR_GROUP_CODE)
+                 .prefetch_related('company')
+                 .order_by('name'),
+            page=page_bdr
+        )
 
     def get_fgas(self):
-        return Person.objects.filter(
-            company__group__code__in=FGASES_GROUP_CODE)
+        page_fgas = self.request.GET.get('page_fgas', 1)
+        return self.get_current_page(
+            data=Person.objects
+                .filter(company__group__code__in=FGASES_GROUP_CODE)
+                .prefetch_related('company')
+                .order_by('name'),
+            page=page_fgas
+        )
 
     def breadcrumbs(self):
         breadcrumbs = super(PersonsView, self).breadcrumbs()
