dbbebcbe9f0bfc6716a29683969296db319ff996
catalinjitea <catalinjitea@gmail.com>
2017-08-24 17:16:15 +0300

    [refs #87505] Add test for sent notifications for each company

--- notifications/tests/test_company.py
+++ notifications/tests/test_company.py
@@ -13,3 +13,27 @@ class CompanyTest(BaseTest):
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(len(resp.context['groups']), 1)
         self.assertEqual(resp.context['items'][0], company)
+
+
+class SentNotificationsTest(BaseTest):
+
+    def test_sent_notification(self):
+        group = factories.CompaniesGroupFactory()
+        company = factories.CompanyFactory(group=group)
+        stage = factories.StageFactory(can_edit=True)
+        cycle = factories.CycleFactory(stage=stage)
+        emailtemplate = factories.EmailTemplateFactory(group=group,
+                                                       stage=stage)
+        cycle_emailtemplate = factories.CycleEmailTemplateFactory(emailtemplate=emailtemplate,
+                                                                  cycle=cycle)
+
+        resp = self.client.get(
+            reverse('notifications:template:sent_notifications',
+                    kwargs={
+                        'pk': cycle_emailtemplate.pk,
+                        'pk_company': company.pk,
+                    })
+        )
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.context['company'], company)
+        self.assertEqual(len(resp.context['persons_info']), 0)
