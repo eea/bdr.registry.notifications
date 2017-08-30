46c584ad3963d826814ff3c1b12f80e2d486abd9
Diana Boiangiu <boiangiudiana58@gmail.com>
2017-08-30 17:47:27 +0300

    [refs #87807] Fix unicode error on parsing fgases

--- notifications/management/commands/fetch_fgases.py
+++ notifications/management/commands/fetch_fgases.py
@@ -41,6 +41,8 @@ class Command(BaseFetchCommand, BaseCommand):
 
     def parse_person_data(self, person):
         fmt_person_name = '{contact_firstname} {contact_lastname}'
+        for key in person.keys():
+            person[key]=person[key].encode('utf-8')
         person_name = fmt_person_name.format(**person)
         return dict(
             username=person['username'],
