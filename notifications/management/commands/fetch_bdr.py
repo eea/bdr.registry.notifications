d15aaa96a8e5a080ea6224215bb2a0e51d9b6bfb
dianaboiangiu <boiangiudiana58@gmail.com>
2020-08-14 18:08:33 +0300

    [refs #120320] Fix broken test

--- notifications/management/commands/fetch_bdr.py
+++ notifications/management/commands/fetch_bdr.py
@@ -50,7 +50,7 @@ class Command(BaseFetchCommand, BaseCommand):
             person[key] = person[key]
         return dict(
             username=person['contactemail'],
-            name=person['contactname'].strip('None').strip(),
+            name=person['contactname'].replace('None', '').strip(),
             email=person['contactemail'],
         )
 
